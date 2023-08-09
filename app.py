617ca9f4f8f2ef5037fc2df4c2a723210045faf5
Saurabh Shinde <123809215+MRSaurabh27@users.noreply.github.com>
2023-08-09 23:17:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-app = Flask(__name__)
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    print("Hello world")
-    return 'Hello, World!'
+def run_server():
+    server_address = ('0.0.0.0', 8000)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print('Server is running...')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    run_server()
+
 
     
